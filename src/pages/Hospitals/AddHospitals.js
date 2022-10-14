25d12c5c8dee1aa8f1bf11b1de463a65792f42c4
ravi-digiqt <ravi@digiqt.com>
2022-10-14 12:29:44 +0530

    dialog ui design

--- src/pages/Hospitals/AddHospitals.js
+++ src/pages/Hospitals/AddHospitals.js
@@ -1,35 +1,109 @@
 import * as React from 'react';
-import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogContentText, DialogActions } from '@mui/material';
-
-import { PlusOutlined } from '@ant-design/icons';
-import Typography from 'themes/overrides/Typography';
+// import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogContentText, DialogActions, Grid, Box } from '@mui/material';
+import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormLabel,
+    Grid,
+    IconButton,
+    Stack,
+    TextField,
+    Typography
+} from '@mui/material';
+// import Typography from 'themes/overrides/Typography';
 
 function AddHospitals() {
-    const [open, setOpen] = React.useState(false);
+    const [state, setState] = React.useState({
+        open: false
+    });
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    const NotaskClickOpen = () => {
+        setState((prevState) => ({ ...prevState, open: true }));
     };
 
-    const handleClose = (value) => {
-        setOpen(false);
+    const NotaskhandleClose = () => {
+        setState((prevState) => ({ ...prevState, open: false }));
     };
 
     return (
         <>
-            <Button variant="contained" size="large" startIcon={<PlusOutlined />} onClick={handleClickOpen}>
+            <Button variant="contained" size="large" startIcon={<PlusOutlined />} onClick={NotaskClickOpen}>
                 Add Hospitals
             </Button>
-            <Dialog maxWidth={'sm'} open={open} sx={{ width: '100%' }}>
-                <DialogTitle>
-                    <h1>Head</h1>
-                </DialogTitle>
+            <Dialog
+                open={state.open}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                fullWidth={true}
+                maxWidth="sm"
+            >
+                <Box>
+                    <DialogTitle id="alert-dialog-title" sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Typography variant="h3">New Hospital</Typography>
+                        <IconButton onClick={NotaskhandleClose}>
+                            <CloseOutlined />
+                        </IconButton>
+                    </DialogTitle>
+                </Box>
                 <DialogContent>
-                    <h2>Data</h2>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <Stack>
+                                <FormLabel required id="demo-radio-buttons-group-label">
+                                    Hospital name
+                                </FormLabel>
+                                <TextField r />
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Stack direction="row" spacing={2}>
+                                <Stack direction="column" sx={{ width: '100%' }}>
+                                    <FormLabel required id="demo-radio-buttons-group-label">
+                                        State
+                                    </FormLabel>
+                                    <TextField fullWidth />
+                                </Stack>
+                                <Stack direction="column" sx={{ width: '100%' }}>
+                                    <FormLabel required id="demo-radio-buttons-group-label">
+                                        District
+                                    </FormLabel>
+                                    <TextField fullWidth />
+                                </Stack>
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Stack>
+                                <FormLabel required id="demo-radio-buttons-group-label">
+                                    Doctor name 1
+                                </FormLabel>
+                                <TextField />
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Stack>
+                                <FormLabel id="demo-radio-buttons-group-label">Doctor name 2</FormLabel>
+                                <TextField />
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <Stack>
+                                <FormLabel required id="demo-radio-buttons-group-label">
+                                    google map URL
+                                </FormLabel>
+                                <TextField />
+                            </Stack>
+                        </Grid>
+                    </Grid>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>save</Button>
+                    <Button variant="contained" size="large">
+                        Save
+                    </Button>
                 </DialogActions>
             </Dialog>
         </>
